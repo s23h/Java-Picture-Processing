622fdce968d8f2ca76369058b50e4271d554b2c8
Salman Hussain <salman@dyn1215-64.wlan.ic.ac.uk>
2018-01-15 23:23:24 +0000

    blur not working

--- src/picture/Main.java
+++ src/picture/Main.java
@@ -24,6 +24,38 @@ public class Main {
           p.save(args[3]);
    }
 
+   if (args[0].equals("flip")) {
+      Process p = new Process();
+      p.setPicture(args[2]);
+      p.flip(args[1]);
+      p.save(args[3]);
+    }
+
+
+    if (args[0].equals("blend")) {
+      Process p = new Process();
+      Picture[] pics = new Picture[args.length-2];
+      for (int i=0; i<args.length-2; i++) {
+        pics[i] = Utils.loadPicture(args[i+1]);
+      }
+      p.blend(pics);
+      p.save(args[args.length-1]);
+    }
+
+    if (args[0].equals("blur")) {
+      Process p = new Process();
+      p.setPicture(args[1]);
+      p.blur();
+      p.save(args[2]);
+    }
+
+
+
+
+
+
+
+
 
   }
 }
--- src/picture/Process.java
+++ src/picture/Process.java
@@ -50,10 +50,23 @@ public class Process {
             for (int i=0; i<h; i++){
                 for (int j=0; j<w; j++) {
                     Color color = picture.getPixel(j, i);
-                    out.setPixel((w-j), i, color);
+                    out.setPixel((w-j-1), i, color);
                 }
             }
         }
+
+      if (c.equals("V")){
+        for (int i=0; i<h; i++){
+          for (int j=0; j<w; j++) {
+            Color color = picture.getPixel(j, i);
+            out.setPixel(j, (h-i-1), color);
+          }
+        }
+      }
+
+      picture = out;
+
+
     }
 
     public static void rotate(int angle) {
@@ -67,22 +80,87 @@ public class Process {
                     out.setPixel((h - 1 - i), j, color);
                 }
             }
+        } else if (angle ==180) {
+            rotate(90);
+            rotate(90);
+        } else if (angle ==270) {
+            rotate(180);
+            rotate(90);
         }
-
-        if (angle ==180) {
-            for (int i=0; i<h; i++) {
-                for (int j = 0; j < w; j++) {
-                    Color color = picture.getPixel(j, i);
-                    out.setPixel((h - 1 - i), j, color);
-                }
-            }
-        }
-
-
-
 
         picture = out;
     }
+
+    public static void blend(Picture[] pics) {
+      int w = smallestWidth(pics);
+      int h = smallestHeight(pics);
+      int n = pics.length;
+      Picture out = Utils.createPicture(w, h);
+      for (int i=0;i<w;i++) {
+        for (int j=0;j<h;j++) {
+          int red = 0;
+          int green = 0;
+          int blue = 0;
+          for (int k=0; k<n; k++) {
+            red += pics[k].getPixel(i, j).getRed();
+            green += pics[k].getPixel(i, j).getGreen();
+            blue += pics[k].getPixel(i, j).getBlue();
+          }
+          Color color = new Color(red/n ,green/n,blue/n);
+          out.setPixel(i, j, color );
+        }
+      }
+
+      picture = out;
+
+    }
+
+    private static int smallestWidth(Picture[] pics) {
+      int w = 0;
+      for (int i=0; i<pics.length;i++) {
+        if (pics[i].getWidth() > 0) {
+          w = pics[i].getWidth();
+        }
+      } return w;
+    }
+
+    private static int smallestHeight(Picture[] pics) {
+      int h = 0;
+      for (int i=0; i<pics.length;i++) {
+        if (pics[i].getHeight() > 0) {
+          h = pics[i].getHeight();
+        }
+      } return h;
+  }
+
+  public static void blur() {
+    int h = picture.getHeight();
+    int w = picture.getWidth();
+    Picture out = picture;
+
+    for (int i=1; i<h-1; i++) {
+      for (int j=1; j<w-1; j++) {
+        int red=0;
+        int green=0;
+        int blue=0;
+        for (int k=i-1; k<i+2; k++) {
+          for (int l=j-1; l<j+2;l++) {
+            red += picture.getPixel(l, k).getRed();
+            green+= picture.getPixel(l, k).getGreen();
+            blue += picture.getPixel(l, k).getBlue();
+          }
+        }
+        Color color = new Color(red/9, green/9, blue/9);
+        out.setPixel(j, i, color);
+      }
+    }
+
+    picture = out;
+
+
+
+
+  }
 
 
 
